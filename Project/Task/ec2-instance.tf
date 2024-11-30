2f918642ccd3b04f5e08dcc8b10738e7ac72cb8a
Gangadhar <gangadharkalaga@gmail.com>
2024-11-30 11:47:38 +0530

    task files

--- Project/Task/ec2-instance.tf
+++ Project/Task/ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web_instance" {
   ami = "ami-022ce6f32988af5fa"
   instance_type = "t2.micro"
   key_name = "DevOps-Key"
-  vpc_security_group_ids = [ "sg-09dd6d33e08227e43" ]
+  vpc_security_group_ids = [ "sg-066c96337c17075b2" ]
   tags = {
     Name = "Webserver"
   }
